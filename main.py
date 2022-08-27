8e825c74c405f202a563b0307260e3b5cf24f4b1
Eobsv <d.eobsv@gamail.com>
2022-08-27 14:20:08 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import turtle as t
+
+tim = t.Turtle()
+screen = t.Screen()
+t.colormode(255)
+
+
+def random_color():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    color = (red, green, blue)
+    return color
+
+
+colour = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+tim.pensize(1)
+tim.speed(500)
+
+
+def draw_sprigraph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_sprigraph(5)
+screen.exitonclick()
